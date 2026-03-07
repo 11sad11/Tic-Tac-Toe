4abe692db01804409517e8c75668ff7e43032369
sad <s10432063@gmail.com>
2026-03-08 02:06:51 +0300

    Add check winner

--- Assets/Project/Scripts/Player/PlayerGameSystem.cs
+++ Assets/Project/Scripts/Player/PlayerGameSystem.cs
@@ -9,14 +9,15 @@ public class PlayerGameSystem : Singleton <PlayerGameSystem>
     public Sprite sprite1 = null;
     public Sprite sprite2 = null;
 
-    private Sprite _sprite;
+    private Sprite _currentPlayerSprite = null;
+    private int[] _cellStates = new int[9];
 
 
     void Start()
     {
         if (sprite1 == null && sprite2 == null) return;
 
-        _sprite = sprite1;
+        _currentPlayerSprite = sprite1;
     }
 
     public void OnClick(int indexButton)
@@ -34,12 +35,48 @@ public class PlayerGameSystem : Singleton <PlayerGameSystem>
 
         //Set sptite 
         Image image = gameObject.GetComponent<Image>();
-        image.sprite = _sprite;
+        image.sprite = _currentPlayerSprite;
+
+        
+        _cellStates[indexButton] =  _currentPlayerSprite == sprite1 ? 1 : 2;
+        if (CheckWinner() == true) Debug.Break(); 
         Switch();
     }
 
     private void Switch()
     {
-        _sprite = _sprite == sprite1 ? sprite2 : sprite1;
+        _currentPlayerSprite = _currentPlayerSprite == sprite1 ? sprite2 : sprite1;
+    }
+
+    private bool CheckWinner()
+    {
+        int[][] winCombos = new int[][]
+        {
+            new int[]{0,1,2},
+            new int[]{3,4,5},
+            new int[]{6,7,8},
+            new int[]{0,3,6},
+            new int[]{1,4,7},
+            new int[]{2,5,8},
+            new int[]{0,4,8},
+            new int[]{2,4,6},
+        };
+
+        foreach (var combo in winCombos)
+        {
+           int a = combo[0];
+           int b = combo[1];
+           int c = combo[2];
+
+            if (_cellStates[a] != 0 &&
+                _cellStates[a] == _cellStates[b] &&
+                _cellStates[a] == _cellStates[c])
+            {
+                Debug.Log("Player " + _cellStates[a] + " wins");
+                return true;
+            }
+        }
+
+        return false;
     }
 }
