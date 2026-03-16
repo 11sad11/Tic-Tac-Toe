def76f1edd9b168ec4cc3fecf34d41486e492634
sad <s10432063@gmail.com>
2026-03-16 17:19:48 +0300

    Edit Singleton

--- Assets/Project/Scripts/Core/Patterns/Singleton.cs
+++ Assets/Project/Scripts/Core/Patterns/Singleton.cs
@@ -2,22 +2,17 @@ using UnityEngine;
 
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    private static T _instance;
-    public static T Instance
-    {
-        get
-        {
-            if (_instance == null)
-                _instance = FindFirstObjectByType<T>();
-            return _instance;
-        }
-    }
+    public static T Instance { get; private set; }
+    protected virtual bool isPersistent => true;
 
     protected virtual void Awake()
     {
-        if (_instance == null)
-            _instance = this as T;
-        else if (_instance != this)
+        if (Instance == null)
+        {
+            Instance = this as T;
+            if (isPersistent) DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
             Destroy(gameObject);
     }
 }
