e3a5c78f4678626337ee9897d628f83329d2e559
sad <s10432063@gmail.com>
2026-03-22 21:40:04 +0300

    added a lot of bullshit

--- Assets/Project/Scripts/Systems/BootstrapperSystem.cs
+++ Assets/Project/Scripts/Systems/BootstrapperSystem.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class BootstrapperSystem : MonoBehaviour
 {
+    public GameObject Background = null;
+
     private void Start()
     {
+        DontDestroyOnLoad(Background);
         //Ожидание конца анимации после чего открывается меню
         SceneLoader.Instance.LoadMainMenu();
     }
